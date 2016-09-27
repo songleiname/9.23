eda7c38650d8d116c53ce387a59d67292e8e006f
song lei <435811453@qq.com>
2016-09-27 11:43:55 +0800

    ff

---
+++ 9.23button.js
@@ -0,0 +1,12 @@
+(function(){
+	var btn1 = document.getElementById('button1');
+	var but = document.getElementsByTagName("button")[3];
+	var button = but[0];
+	/*button.onclick = function (){
+		
+	}*/
+	console.log(but);
+	
+	var buttonname = document.getElementsByName('buttonname1')
+	console.log(buttonname)
+}(window))
